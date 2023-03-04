5f30b7bf5f484f73538c2daa5220d7ba635816b1
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-03 21:46:45 -0300

    create/ index import entities

---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+import { User } from './files/user.entity';
+import { Schedule } from './files/schedule.entity';
+import { RealEstate } from './files/realEstate.entity';
+import { Category } from './files/category.entity';
+import { Address } from './files/address.entity';
+
+export { Address, RealEstate, Category, User, Schedule };
