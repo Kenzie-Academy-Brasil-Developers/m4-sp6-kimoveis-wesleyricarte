9947f51e3ccceb2b9403450482b546eba4f35749
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-07 20:47:02 -0300

    update/ RealEstate entity columns

--- src/entities/files/realEstate.entity.ts
+++ src/entities/files/realEstate.entity.ts
@@ -1,10 +1,28 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+	Entity,
+	Column,
+	PrimaryGeneratedColumn,
+	CreateDateColumn,
+	UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('real_estate')
 export class RealEstate {
-	@PrimaryGeneratedColumn()
+	@PrimaryGeneratedColumn('increment')
 	id: number;
 
-	@Column()
-	name: string;
+	@Column({ default: false })
+	sold: boolean;
+
+	@Column({ type: 'decimal', precision: 10, scale: 2 })
+	value: number | string;
+
+	@Column({ type: 'integer' })
+	size: number;
+
+	@CreateDateColumn({ type: 'timestamp' })
+	createdAt: Date | string;
+
+	@UpdateDateColumn({ type: 'timestamp' })
+	updatedAt: Date | string;
 }
