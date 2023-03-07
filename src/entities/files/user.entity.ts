8c283919f8ef987df710a6922d90671234d517ab
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-07 20:47:39 -0300

    update/ User entity columns

--- src/entities/files/user.entity.ts
+++ src/entities/files/user.entity.ts
@@ -1,10 +1,45 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { getRounds, hashSync } from 'bcryptjs';
+import {
+	Entity,
+	Column,
+	PrimaryGeneratedColumn,
+	CreateDateColumn,
+	UpdateDateColumn,
+	DeleteDateColumn,
+    BeforeInsert,
+    BeforeUpdate,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
-	@PrimaryGeneratedColumn()
+	@PrimaryGeneratedColumn('increment')
 	id: number;
 
-	@Column()
+	@Column({ length: 45 })
 	name: string;
+
+	@Column({ length: 45, unique: true })
+	email: string;
+
+	@Column({ default: false })
+	admin: boolean;
+
+	@Column({ length: 120 })
+	password: string;
+
+	@CreateDateColumn({ type: 'timestamp' })
+	createdAt: string;
+
+	@UpdateDateColumn({ type: 'timestamp' })
+	updatedAt: string;
+
+	@DeleteDateColumn({ type: 'timestamp' })
+	deletedAt: string;
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword(){
+        const isEncrypted = getRounds(this.password)
+        if (!isEncrypted) this.password = hashSync(this.password, 10)
+    }
 }
