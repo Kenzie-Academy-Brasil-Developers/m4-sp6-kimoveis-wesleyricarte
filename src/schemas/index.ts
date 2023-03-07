3af309e56e1e632c0ddc6446dd03c2781171798c
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-07 20:49:48 -0300

    create/ schemas imports

---
+++ src/schemas/index.ts
@@ -0,0 +1,17 @@
+import {
+	defaultUserSchema,
+	createUserSchema,
+	returnUserSchema,
+	updateUserSchema,
+} from './files/users.schemas';
+
+import { defaultLoginSchema, returnLoginSchema } from './files/login.schemas';
+
+export {
+	defaultUserSchema,
+	createUserSchema,
+	returnUserSchema,
+	updateUserSchema,
+	defaultLoginSchema,
+	returnLoginSchema,
+};
