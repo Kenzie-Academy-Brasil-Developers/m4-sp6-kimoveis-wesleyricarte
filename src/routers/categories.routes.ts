4ca3531265125086552f37dcf70a497db6551ed6
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-08 20:47:59 -0300

    update/ adding controller to get route

--- src/routers/categories.routes.ts
+++ src/routers/categories.routes.ts
@@ -1,4 +1,4 @@
-import { createCategoryController } from './../controllers/files/categories.controllers';
+import { createCategoryController, readCategoriesController } from '../controllers';
 import { Router } from 'express';
 import { createCategorySchema } from '../schemas/';
 import { adminTokenMiddleare, validDataMiddleare } from '../middlewares';
@@ -6,7 +6,7 @@ import { adminTokenMiddleare, validDataMiddleare } from '../middlewares';
 const categoriesRoutes: Router = Router();
 
 categoriesRoutes.post('', adminTokenMiddleare, validDataMiddleare(createCategorySchema), createCategoryController);
-categoriesRoutes.get('');
+categoriesRoutes.get('', readCategoriesController);
 categoriesRoutes.get('/:id/realEstate');
 
 export default categoriesRoutes;
