ddce746b7806c2fb5aaaa288409b4ac03dc754ed
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-08 16:26:33 -0300

    update/ Services imports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,13 @@
-import loginService from "./login/login.service";
-
+import loginService from './login/login.service';
+import createUsersService from './users/create.service';
+import readUsersService from './users/read.service';
+import updateUsersService from './users/update.service';
+import deleteUsersService from './users/delete.service';
 
 export {
-    loginService
-}
+	loginService,
+	createUsersService,
+	readUsersService,
+	updateUsersService,
+	deleteUsersService,
+};
