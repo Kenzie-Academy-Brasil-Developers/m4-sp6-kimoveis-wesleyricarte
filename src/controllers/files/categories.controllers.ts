7147cc3bafc91d8a57eca76f9745ada68e6fdacf
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-08 20:47:43 -0300

    update/ read categories controller

--- src/controllers/files/categories.controllers.ts
+++ src/controllers/files/categories.controllers.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from 'express';
 import { iCategory } from '../../interfaces';
-import { createCategoryService } from '../../services';
+import { createCategoryService, readCategoriesService } from '../../services';
 
 export const createCategoryController = async (req: Request, res: Response): Promise<Response> => {
 	const newCategory: iCategory = await createCategoryService(req.body);
 
 	return res.status(201).json(newCategory);
 };
+
+export const readCategoriesController = async (req: Request, res: Response): Promise<Response> => {
+    const categories: Array<iCategory> = await readCategoriesService()
+
+    return res.status(200).json(categories)
+}
