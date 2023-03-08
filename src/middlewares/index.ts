b48e74a0fc72bdec4763f4cb0758d2d58ba4b017
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-07 21:33:59 -0300

    update/ Middlewares Imports

---
+++ src/middlewares/index.ts
@@ -0,0 +1,13 @@
+import adminTokenMiddleare from './files/adminToken.middleware';
+import duplicatedUserMiddleare from './files/duplicatedUser.middleware';
+import validUserMiddleare from './files/validUser.middleware';
+import validDataMiddleare from './files/validData.middleware';
+import validTokenMiddleare from './files/validToken.middleware';
+
+export {
+	adminTokenMiddleare,
+	duplicatedUserMiddleare,
+	validUserMiddleare,
+	validDataMiddleare,
+	validTokenMiddleare,
+};
