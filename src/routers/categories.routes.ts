465c1aeade08bbe09b10b4c24967894b5d59a249
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-08 20:28:41 -0300

    update/ categories post route

--- src/routers/categories.routes.ts
+++ src/routers/categories.routes.ts
@@ -1,9 +1,11 @@
+import { createCategoryController } from './../controllers/files/categories.controllers';
 import { Router } from 'express';
+import { createCategorySchema } from '../schemas/';
 import { adminTokenMiddleare, validDataMiddleare } from '../middlewares';
 
 const categoriesRoutes: Router = Router();
 
-categoriesRoutes.post('', adminTokenMiddleare, validDataMiddleare(schema));
+categoriesRoutes.post('', adminTokenMiddleare, validDataMiddleare(createCategorySchema), createCategoryController);
 categoriesRoutes.get('');
 categoriesRoutes.get('/:id/realEstate');
 
