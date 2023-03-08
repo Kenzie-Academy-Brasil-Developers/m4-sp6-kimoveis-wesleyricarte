655f0058eed1db9708ecbba6eecf7b93d5a2ce51
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-08 16:22:35 -0300

    update/ Interfaces imports

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,17 @@
 import { iLoginRequest } from './files/login.interfaces';
+import {
+	iUser,
+	iUserCreate,
+	iUserUpdate,
+	iUserReturn,
+	iUserReturnArray,
+} from './files/users.interfaces';
 
-export { iLoginRequest };
+export {
+	iLoginRequest,
+	iUser,
+	iUserCreate,
+	iUserUpdate,
+	iUserReturn,
+	iUserReturnArray,
+};
