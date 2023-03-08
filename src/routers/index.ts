6c150ebd8276d28f53fae3c8432c08a8efd31aca
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-07 21:30:00 -0300

    create/ Routes Imports

---
+++ src/routers/index.ts
@@ -0,0 +1,13 @@
+import categoriesRoutes from './categories.routes';
+import loginRoutes from './login.routes';
+import realEstateRoutes from './realEstate.routes';
+import schedulesRoutes from './schedules.routes';
+import usersRoutes from './users.routes';
+
+export {
+	categoriesRoutes,
+	loginRoutes,
+	realEstateRoutes,
+	schedulesRoutes,
+	usersRoutes,
+};
