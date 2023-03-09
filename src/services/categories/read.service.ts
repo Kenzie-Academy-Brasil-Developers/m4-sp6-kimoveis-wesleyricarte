80d9e56cd4324792243110c314dae9d22b448142
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-09 16:25:04 -0300

    update/ adding read RealEstate ByCategories Servic

--- src/services/categories/read.service.ts
+++ src/services/categories/read.service.ts
@@ -1,15 +1,25 @@
 import { Repository } from 'typeorm';
 import { AppDataSource } from '../../data-source';
-import { Category } from '../../entities';
+import { Category, RealEstate } from '../../entities';
+import { AppError } from '../../errors';
 import { iCategory } from '../../interfaces';
 
-const readCategoriesService = async (): Promise<Array<iCategory>> => {
-	const categoriesRepository: Repository<Category> =
-		AppDataSource.getRepository(Category);
+export const readCategoriesService = async (): Promise<Array<iCategory>> => {
+	const categoriesRepository: Repository<Category> = AppDataSource.getRepository(Category);
 
 	const categories: Array<iCategory> = await categoriesRepository.find();
 
 	return categories;
 };
 
-export default readCategoriesService;
+export const readRealEstateByCategoriesService = async (categoryId: number): Promise<Category> => {
+    const categoriesRepository: Repository<Category> = AppDataSource.getRepository(Category);
+	const realEstateRepository: Repository<RealEstate> = AppDataSource.getRepository(RealEstate);
+
+    const category: iCategory | null = await categoriesRepository.findOneBy({ id: categoryId });
+    if (!category) {throw new AppError('Category not found', 404)}
+
+	const realEstates: Category | null = await categoriesRepository.findOne({ where: {id: category.id!}, relations: { realEstate: true } });
+
+	return realEstates!
+};
