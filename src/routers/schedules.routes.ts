045fad844a26d68a4ea65bf9abd2b24f99489193
Wesley Ricarte <wesley.ricarte97@gmail.com>
2023-03-10 17:16:54 -0300

    update/ schedules routes middlewares

--- src/routers/schedules.routes.ts
+++ src/routers/schedules.routes.ts
@@ -1,15 +1,11 @@
 import { Router } from 'express';
-import {
-	adminTokenMiddleare,
-	validDataMiddleare,
-	validTokenMiddleare,
-} from '../middlewares';
+import { createScheduleSchema } from '../schemas';
+import { createSchedulesController, readSchedulesController } from '../controllers';
+import { adminTokenMiddleare, validDataMiddleare, validTokenMiddleare } from '../middlewares';
 
 const schedulesRoutes: Router = Router();
 
-schedulesRoutes.post('', validTokenMiddleare,
-// validDataMiddleare(schema)
-);
-schedulesRoutes.get('', adminTokenMiddleare);
+schedulesRoutes.post('', validTokenMiddleare, validDataMiddleare(createScheduleSchema), createSchedulesController);
+schedulesRoutes.get('/realEstate/:id', adminTokenMiddleare, readSchedulesController);
 
 export default schedulesRoutes;
